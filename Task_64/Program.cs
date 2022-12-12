bf9a8c560b25414520af1aa01081a218121bd17a
smolskaya tatyana <smolskayatatyana@yandex.ru>
2022-12-12 11:47:42 +0300

    Задача 64 решена

---
+++ Task_64/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+System.Console.WriteLine(PrintNumbers(10, 1));
+string PrintNumbers(int end, int start)
+{
+    if (end == start)
+    {
+        return end.ToString();
+    }
+    return (end + " " + PrintNumbers(--end, start));
+}
